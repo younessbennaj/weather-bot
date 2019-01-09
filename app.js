f1afe4e4d994230553094c174ee28d04ddf1ffbf
Youness BENNAJ <youness.bennaj@gmail.com>
2019-01-09 15:27:57 +0100

    add feature to send weather for a specific day

--- app.js
+++ app.js
@@ -21,29 +21,88 @@ app.get("/", (req, res) => {
 his search criterias*/
 app.post("/weather", (req, res) => {
   const { location } = req.body.conversation.memory;
-
   if (req.body.nlp.entities.datetime) {
-    const datetime = req.body.nlp.entities.datetime[0].iso;
+    if (req.body.nlp.entities.datetime[0].accuracy == "day") {
+      const datetime = req.body.nlp.entities.datetime[0].iso;
 
-    //Access to 5 day forecast for any location or city
-    axios
-      .get(API_FORECAST_URL, {
-        params: {
-          lat: location.lat,
-          lon: location.lng,
-          appid: API_KEY
-        }
-      })
-      .then(response => {
-        //Access weather for a specific day
-        const weatherOneDay = response.data.list.filter((element, index) => {
-          let actuelDay = new Date(datetime).getDate();
-          let dateElement = new Date(element.dt_txt).getDate();
-          if (dateElement == actuelDay) {
-            return element;
+      //Access to 5 day forecast for any location or city
+      axios
+        .get(API_FORECAST_URL, {
+          params: {
+            lat: location.lat,
+            lon: location.lng,
+            appid: API_KEY
           }
+        })
+        .then(response => {
+          //Access weather for a specific day
+          const weatherOneDay = response.data.list.filter((element, index) => {
+            let actuelDay = new Date(datetime).getDate();
+            let dateElement = new Date(element.dt_txt).getDate();
+            if (dateElement == actuelDay) {
+              return element;
+            }
+          });
+
+          const weatherOneHour = weatherOneDay.filter((element, index) => {
+            let actualHour = 12;
+            let hourElement = new Date(element.dt_txt).getHours();
+            if (actualHour == hourElement) {
+              return element;
+            }
+          });
+
+          console.log(weatherOneHour[0].weather);
+
+          getWeatherImage(weatherOneHour[0].weather[0].id).then(result => {
+            const weatherUrl = result;
+
+            res.json({
+              replies: [
+                {
+                  type: "card",
+                  content: {
+                    title: weatherOneHour[0].weather[0].main,
+                    subtitle: location.formatted,
+                    imageUrl: weatherUrl,
+                    buttons: [
+                      {
+                        title: "Matin",
+                        value: `Quel temps fait il à ${
+                          location.raw
+                        } demain matin ?`
+                      },
+                      {
+                        title: "Midi",
+                        value: `Quel temps fait il à ${
+                          location.raw
+                        } demain midi ?`
+                      },
+                      {
+                        title: "Soir",
+                        value: `Quel temps fait il à ${
+                          location.raw
+                        } demain soir ?`
+                      }
+                    ]
+                  }
+                }
+              ]
+            });
+          });
         });
+    }
+
+    if (req.body.nlp.entities.datetime[0].accuracy == "day,halfday") {
+      res.json({
+        replies: [
+          {
+            type: "text",
+            content: "HALFDAY weather"
+          }
+        ]
       });
+    }
   } else {
     axios
       .get(API_URL, {
@@ -63,7 +122,7 @@ app.post("/weather", (req, res) => {
                 type: "card",
                 content: {
                   title: main,
-                  subtitle: "subtitle",
+                  subtitle: location.formatted,
                   imageUrl: weatherUrl,
                   buttons: [
                     {
